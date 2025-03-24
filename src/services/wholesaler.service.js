ef6c0742a224c809ae4b57d21700018e1d9d7ee1
Jaser <syedahmedullahjaser@gmail.com>
2025-03-24 08:37:46 +0530

    Implemented getMonthlyTurnover in wholesaler service

--- src/services/wholesaler.service.js
+++ src/services/wholesaler.service.js
@@ -20,6 +20,32 @@ class WholesalerService {
     }
   }
   
+  // API 3: To Get monthly turnover for each wholesaler for a year
+  async getMonthlyTurnover(year = 2021) {
+    try {
+      const result = await Stock.findAll({
+        attributes: [
+          [sequelize.fn('EXTRACT', sequelize.literal('MONTH FROM "date"')), 'month'],
+          'wholesaler_id',
+          [sequelize.fn('SUM', sequelize.col('stock_amount')), 'total_turnover']
+        ],
+        where: sequelize.where(
+          sequelize.fn('EXTRACT', sequelize.literal('YEAR FROM "date"')),
+          year
+        ),
+        group: ['wholesaler_id', sequelize.fn('EXTRACT', sequelize.literal('MONTH FROM "date"'))],
+        order: ['wholesaler_id', [sequelize.fn('EXTRACT', sequelize.literal('MONTH FROM "date"')), 'ASC']],
+        include: [{
+          model: Wholesaler,
+          attributes: ['name']
+        }]
+      });
+      
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
 
 }
 
